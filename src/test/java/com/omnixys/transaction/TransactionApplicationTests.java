d506483917c6b29aa9c0adfb19c59df172fd8a76
Caleb-Script <caleb-script@outlook.de>
2025-06-11 14:18:08 +0200

    Update TransactionApplicationTests.java

--- src/test/java/com/omnixys/transaction/TransactionApplicationTests.java
+++ src/test/java/com/omnixys/transaction/TransactionApplicationTests.java
@@ -1,5 +1,7 @@
 package com.omnixys.transaction;
 
+import com.omnixys.transaction.config.Env;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -7,6 +9,11 @@ import org.springframework.context.annotation.Import;
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
 class TransactionApplicationTests {
+
+  @BeforeAll
+    protected static void setup() {
+        new Env();
+    }
 
   @Test
   void contextLoads() {
